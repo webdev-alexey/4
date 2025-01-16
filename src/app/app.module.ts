3e81ea38b8f5906c5c166063c8e14725a4b2c9b4
alexey <alexey.anasov@yandex.by>
2025-01-16 21:59:27 +0300

    с адаптивом и лоадером через API

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppModule {}
